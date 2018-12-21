dd9fcdcb6dd62576549d43f70b1675bfda022fc2
Christian Reyes <creyes922@gmail.com>
2018-12-21 13:29:15 -0600

    Started on the grid component.

---
+++ ui/src/components/Grid.vue
@@ -0,0 +1,16 @@
+<template>
+  
+</template>
+
+<script>
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Grid extends Vue {
+
+}
+</script>
+
+<style scoped>
+
+</style>
