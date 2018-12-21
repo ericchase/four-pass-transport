3da0d9301c24b798129127ae4ea13ab8159a7b89
Eric Chase <eric.nalley+github@gmail.com>
2018-12-21 08:21:53 -0600

    added PathSet and getMinimalDistancePath and tests

---
+++ code/test/minimal-distance-paths.test.ts
@@ -0,0 +1,116 @@
+import { expect } from 'chai';
+import 'mocha';
+
+import { Cell } from '../src/grid';
+import { getMinimalDistancePath, PathSet } from '../src/minimal-distance-paths';
+import { AStarGrid } from '../src/a-star-grid';
+
+describe('PathSet', function () {
+  let pathSet: PathSet;
+
+  beforeEach('prepare a Path Set', () => {
+    pathSet = new PathSet(
+      [new Cell(0, 0),
+       new Cell(1, 1),
+       new Cell(2, 2),
+       new Cell(3, 3)], [2, 0, 1]);
+  });
+
+  describe('constructor', function () {
+    it('should populate a pairs list with 3 pairs', () => {
+      expect(pathSet.pairs)
+        .to.be.length(3);
+    });
+    it('should store input cell list as pairs', () => {
+      expect(pathSet.pairs)
+        .to.deep.equal(
+        [
+          [new Cell(0, 0), new Cell(1, 1)],
+          [new Cell(1, 1), new Cell(2, 2)],
+          [new Cell(2, 2), new Cell(3, 3)]
+        ]);
+    });
+    it('should store input number list as order', () => {
+      expect(pathSet.order)
+        .to.deep.equal([2, 0, 1]);
+    });
+  });
+
+  describe('* nextEndpointPair', function () {
+    it('should return endpoint pairs in the order specified by order', () => {
+      let endpointPairs = pathSet.nextEndpointPair();
+
+      expect(endpointPairs.next().value)
+        .to.deep.equal([new Cell(2, 2), new Cell(3, 3)]);
+      expect(endpointPairs.next().value)
+        .to.deep.equal([new Cell(0, 0), new Cell(1, 1)]);
+      expect(endpointPairs.next().value)
+        .to.deep.equal([new Cell(1, 1), new Cell(2, 2)]);
+    });
+  });
+
+  describe('storeNextPath', function () {
+    it('should store paths in the order specified by order', () => {
+      pathSet.storeNextPath([new Cell(2, 2), new Cell(2, 3), new Cell(3, 3)]);
+      pathSet.storeNextPath([new Cell(0, 0), new Cell(0, 1), new Cell(1, 1)]);
+      pathSet.storeNextPath([new Cell(1, 1), new Cell(1, 2), new Cell(2, 2)]);
+
+      expect(pathSet.paths)
+        .to.deep.equal(
+        [
+          [new Cell(2, 2), new Cell(2, 3), new Cell(3, 3)],
+          [new Cell(0, 0), new Cell(0, 1), new Cell(1, 1)],
+          [new Cell(1, 1), new Cell(1, 2), new Cell(2, 2)]
+        ]);
+    });
+    it('should throw error if trying to store more than 3 paths', () => {
+      pathSet.storeNextPath([new Cell(2, 2), new Cell(2, 3), new Cell(3, 3)]);
+      pathSet.storeNextPath([new Cell(0, 0), new Cell(0, 1), new Cell(1, 1)]);
+      pathSet.storeNextPath([new Cell(1, 1), new Cell(1, 2), new Cell(2, 2)]);
+
+      expect(() => pathSet.storeNextPath([new Cell(1, 1), new Cell(1, 2), new Cell(2, 2)]))
+        .to.throw(Error);
+    });
+  });
+
+  describe('getCombinedPaths', function () {
+    it('should return a combined path of paths in order of original end-points', () => {
+      pathSet.storeNextPath([new Cell(2, 2), new Cell(2, 3), new Cell(3, 3)]);
+      pathSet.storeNextPath([new Cell(0, 0), new Cell(0, 1), new Cell(1, 1)]);
+      pathSet.storeNextPath([new Cell(1, 1), new Cell(1, 2), new Cell(2, 2)]);
+
+      expect(pathSet.getCombinedPaths())
+        .to.deep.equal([new Cell(0, 0), new Cell(0, 1), new Cell(1, 1),
+                        new Cell(1, 2), new Cell(2, 2),
+                        new Cell(2, 3), new Cell(3, 3)]);
+    });
+  });
+});
+
+describe('Minimal Distance Path', function () {
+  let pathSet: PathSet;
+  let board: AStarGrid;
+
+  beforeEach('prepare pathSet and board', () => {
+    pathSet = new PathSet(
+      [new Cell(0, 0),
+       new Cell(1, 1),
+       new Cell(2, 2),
+       new Cell(3, 3)], [2, 0, 1]);
+    board = new AStarGrid(10, 10);
+  });
+
+  describe('getMinimalDistancePath', function () {
+    it('should return a path of length 7', () => {
+      expect(getMinimalDistancePath(pathSet))
+        .to.be.length(7);
+    });
+    it('should return a list of unique cells', () => {
+      expect(getMinimalDistancePath(pathSet))
+        .to.include.deep.members([new Cell(0, 0),
+                                  new Cell(1, 1),
+                                  new Cell(2, 2),
+                                  new Cell(3, 3)]);
+    });
+  });
+});
