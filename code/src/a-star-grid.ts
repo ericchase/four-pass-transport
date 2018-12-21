163cb5d60442b613c38137971d539141a6b3b2bb
Eric Chase <eric.nalley+github@gmail.com>
2018-12-21 08:20:19 -0600

    added clearData and getEndpoint to AStarGrid

--- code/src/a-star-grid.ts
+++ code/src/a-star-grid.ts
@@ -7,19 +7,29 @@ export class AStarGrid extends WeightedGrid implements AStarGraph<Cell> {
 
   constructor(readonly rows: number, readonly columns: number) {
     super(rows, columns, manhattanDistance, manhattanDistance);
+    this.clearData();
+  }
 
-    for (let row = this.minRow; row < rows; ++row) {
-      for (let col = this.minCol; col < columns; ++col) {
+  clearData(): void {
+    for (let row = this.minRow; row < this.rows; ++row) {
+      for (let col = this.minCol; col < this.columns; ++col) {
         this.data[this.makeIndex(row, col)] = new AStarData<Cell>();
       }
     }
+  }
+
+  getData(node: Cell): AStarData<Cell> {
+    return this.data[this.getIndex(node)];
   }
 
   getNode(node: Cell): Cell {
     return this.nodes[this.getIndex(node)];
   }
 
-  getData(node: Cell): AStarData<Cell> {
-    return this.data[this.getIndex(node)];
+  getEndpoint(node: Cell): Cell {
+    node = this.getNode(node);
+    if (node !== undefined)
+      this.clearObstacles([node]);
+    return node;
   }
 }
