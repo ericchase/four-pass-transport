199f1d95e3c111de546068b592a3861f056554bc
Eric Chase <eric.nalley+github@gmail.com>
2018-12-21 10:07:59 -0600

    added getAllMinimalDistancePaths and tests, made correction

--- code/src/minimal-distance-paths.ts
+++ code/src/minimal-distance-paths.ts
@@ -2,10 +2,13 @@ import { Cell } from './grid';
 import { aStar } from './a-star';
 import { AStarGrid } from './a-star-grid';
 import { PathSet } from './pathset';
+import { MinHeap } from './min-heap';
+import { threeAryPermutationGenerator } from './permutation-function';
 
 
 export function getMinimalDistancePath(pathSet: PathSet) {
   let board: AStarGrid = new AStarGrid(10, 10);
+  board.setObstacles(pathSet.endpoints);
 
   for (let pair of pathSet.nextEndpointPair()) {
     let path: Cell[] = aStar(pair[0], pair[1], board);
@@ -14,4 +17,15 @@ export function getMinimalDistancePath(pathSet: PathSet) {
   }
 
   return pathSet.getCombinedPaths();
+}
+
+export function getAllMinimalDistancePaths(endpoints: Cell[]): MinHeap<Cell[]> {
+  let minHeap = new MinHeap<Cell[]>((pathA: Cell[], pathB: Cell[]) => {return pathA.length - pathB.length;});
+
+  for (let order of threeAryPermutationGenerator<number>(0, 1, 2)) {
+    let pathSet: PathSet = new PathSet(endpoints, order);
+    minHeap.push(getMinimalDistancePath(pathSet));
+  }
+
+  return minHeap;
 }
--- code/test/minimal-distance-paths.test.ts
+++ code/test/minimal-distance-paths.test.ts
@@ -2,25 +2,26 @@ import { expect } from 'chai';
 import 'mocha';
 
 import { Cell } from '../src/grid';
-import { getMinimalDistancePath } from '../src/minimal-distance-paths';
+import { getAllMinimalDistancePaths, getMinimalDistancePath } from '../src/minimal-distance-paths';
 import { AStarGrid } from '../src/a-star-grid';
 import { PathSet } from '../src/pathset';
+import { MinHeap } from '../src/min-heap';
 
 
-describe('Minimal Distance Path', function () {
-  let pathSet: PathSet;
-  let board: AStarGrid;
-
-  beforeEach('prepare pathSet and board', () => {
-    pathSet = new PathSet(
-      [new Cell(0, 0),
-       new Cell(1, 1),
-       new Cell(2, 2),
-       new Cell(3, 3)], [2, 0, 1]);
-    board = new AStarGrid(10, 10);
-  });
-
+describe('Minimal Distance Paths', function () {
   describe('getMinimalDistancePath', function () {
+    let pathSet: PathSet;
+    let board: AStarGrid;
+
+    beforeEach('prepare pathSet and board', () => {
+      pathSet = new PathSet(
+        [new Cell(0, 0),
+         new Cell(1, 1),
+         new Cell(2, 2),
+         new Cell(3, 3)], [2, 0, 1]);
+      board = new AStarGrid(10, 10);
+    });
+
     it('should return a path of length 7', () => {
       expect(getMinimalDistancePath(pathSet))
         .to.be.length(7);
@@ -31,6 +32,27 @@ describe('Minimal Distance Path', function () {
                                   new Cell(1, 1),
                                   new Cell(2, 2),
                                   new Cell(3, 3)]);
+    });
+  });
+
+  describe('getAllMinimalDistancePaths', function () {
+
+    let minHeap: MinHeap<Cell[]>;
+
+    before('call getAllMinimalDistancePaths', () => {
+      minHeap = getAllMinimalDistancePaths([new Cell(2, 2),
+                                            new Cell(5, 5),
+                                            new Cell(5, 2),
+                                            new Cell(2, 5)]);
+    });
+
+    it('should return a min-heap of 6 minimal distance paths', () => {
+      expect(minHeap.size)
+        .to.equal(6);
+    });
+    it('should return a min-heap with a 20 length path on top', () => {
+      expect(minHeap.top)
+        .to.be.length(20);
     });
   });
 });
