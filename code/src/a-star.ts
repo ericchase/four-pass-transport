13d0d761e84b0fc39485d88eb9038f817b45b5d4
Eric Chase <eric.nalley+github@gmail.com>
2018-12-20 22:29:09 -0600

    updated AStar function and tests

--- code/src/a-star.ts
+++ code/src/a-star.ts
@@ -18,22 +18,21 @@
   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
   SOFTWARE.*/
 
+import { WeightedGraph } from './graph';
 import { MinHeap } from './min-heap';
 
-export class Node<T> {
+export class AStarData<NodeType> {
   g: number = 0;
   h: number = 0;
   f: number = 0;
   opened: boolean = false;
   closed: boolean = false;
-  parent: T | undefined;
+  parent!: NodeType;
 }
 
-export interface Graph<T> {
-  getCost(nodeA: T, nodeB: T): number;
-  getHeuristic(nodeA: T, nodeB: T): number;
-  getMemoizedNode(node: T): T;
-  getNeighbors(node: T): T[];
+export interface AStarGraph<NodeType> extends WeightedGraph<NodeType> {
+  getNode(node: NodeType): NodeType;
+  getData(node: NodeType): AStarData<NodeType>;
 }
 
 /**
@@ -41,62 +40,67 @@ export interface Graph<T> {
  * @return {Array<Array<number>>} The path, including both start and
  *     end positions.
  */
-export function aStar<T extends Node<T>>(startNode: T, endNode: T, graph: Graph<T>) {
-  startNode = graph.getMemoizedNode(startNode);
-  endNode = graph.getMemoizedNode(endNode);
+export function aStar<NodeType>(startNode: NodeType, endNode: NodeType, graph: AStarGraph<NodeType>) {
+  startNode = graph.getNode(startNode);
+  endNode = graph.getNode(endNode);
+
+  if (startNode === undefined || endNode === undefined)
+    return [];
 
   // aka. the frontier or fringe
-  let openList = new MinHeap<T>((cellA, cellB) => cellA.f - cellB.f);
+  let openList = new MinHeap<NodeType>((cellA, cellB) => graph.getData(cellA).f - graph.getData(cellB).f);
 
   // set the `g` and `f` value of the start node to be 0
-  startNode.g = 0;
-  startNode.f = 0;
+  graph.getData(startNode).g = 0;
+  graph.getData(startNode).f = 0;
 
   // push the start node into the open list
   openList.push(startNode);
-  startNode.opened = true;
+  graph.getData(startNode).opened = true;
 
   // while the open list is not empty
   while (!openList.isEmpty()) {
     // pop the position of node which has the minimum `f` value.
-    let node: T = openList.top;
+    let node: NodeType = openList.top;
+    graph.getData(node).closed = true;
     openList.pop();
-    node.closed = true;
 
     // if reached the end position, construct the path and return it
     if (node == endNode) {
-      let path: T[] = [node];
-      while (node.parent) {
-        node = node.parent;
+      let path: NodeType[] = [node];
+
+      while (graph.getData(node).parent != undefined) {
+        node = graph.getData(node).parent;
         path.push(node);
       }
+
       return path.reverse();
     }
 
     // get neighbours of the current node
-    let neighbors: T[] = graph.getNeighbors(node);
+    let neighbors: NodeType[] = graph.neighbors(node);
     for (let i = 0, l = neighbors.length; i < l; ++i) {
-      let neighbor: T = neighbors[i];
+      let neighbor: NodeType = neighbors[i];
 
-      if (neighbor.closed) {
+      if (graph.getData(neighbor).closed) {
         continue;
       }
 
       // get the distance between current node and the neighbor
       // and calculate the next g score
-      let new_g = node.g + graph.getCost(node, neighbor);
+      let new_g = graph.getData(node).g + graph.cost(node, neighbor);
 
       // check if the neighbor has not been inspected yet, or
       // can be reached with smaller cost from the current node
-      if (!neighbor.opened || new_g < neighbor.g) {
-        neighbor.g = new_g;
-        neighbor.h = neighbor.h || graph.getHeuristic(node, endNode);
-        neighbor.f = neighbor.g + neighbor.h;
-        neighbor.parent = node;
+      if (!graph.getData(neighbor).opened || new_g < graph.getData(neighbor).g) {
+        graph.getData(neighbor).g = new_g;
+        graph.getData(neighbor).h = graph.getData(neighbor).h || graph.estimate(neighbor, endNode);
+        graph.getData(neighbor).f = graph.getData(neighbor).g + graph.getData(neighbor).h;
+        graph.getData(neighbor).parent = node;
 
-        if (!neighbor.opened) {
+        if (!graph.getData(neighbor).opened) {
           openList.push(neighbor);
-          neighbor.opened = true;
+          graph.getData(neighbor).opened = true;
         } else {
           // the neighbor can be reached with smaller cost.
           // Since its f value has been updated, we have to
--- code/test/a-star.test.ts
+++ code/test/a-star.test.ts
@@ -1,23 +1,48 @@
 import { expect } from 'chai';
 import 'mocha';
 
-import { Cell } from '../src/cell';
-import { Grid } from "../src/grid";
-import { aStar } from "../src/a-star";
+import { Cell } from '../src/grid';
+import { aStar } from '../src/a-star';
+import { AStarGrid } from '../src/a-star-grid';
 
 describe('A Star', function () {
+  let grid: AStarGrid;
+
+  beforeEach('make 10x10 AStarGrid', () => {
+    grid = new AStarGrid(10, 10);
+  });
+
   describe('aStar', function () {
-    it('should return [] for aStar((0,0), (-1,-1))', () => {
-      expect(Grid.cleanCells(aStar(new Cell(0, 0), new Cell(-1, -1), new Grid())))
+    it('should return empty array when cell is out of bounds', () => {
+      expect(aStar(new Cell(0, 0),
+                   new Cell(-1, -1),
+                   grid))
         .to.be.empty;
     });
     it('should return [(0,0)] for aStar((0,0), (0,0))', () => {
-      expect(Grid.cleanCells(aStar(new Cell(0, 0), new Cell(0, 0), new Grid())))
+      expect(aStar(new Cell(0, 0),
+                   new Cell(0, 0),
+                   grid))
         .to.deep.equal([new Cell(0, 0)]);
     });
+    it('should return [(1,1),(1,2)] for aStar((1,1), (1,2))', () => {
+      expect(aStar(new Cell(1, 1),
+                   new Cell(1, 2),
+                   grid))
+        .to.deep.equal([new Cell(1, 1),
+                        new Cell(1, 2)]);
+    });
     it('should return [(1,1),(1,2),(1,3)] for aStar((1,1), (1,3))', () => {
-      expect(Grid.cleanCells(aStar(new Cell(1, 1), new Cell(1, 3), new Grid())))
-        .to.deep.equal([new Cell(1, 1), new Cell(1, 2), new Cell(1, 3)]);
+      expect(aStar(new Cell(1, 1),
+                   new Cell(1, 3),
+                   grid))
+        .to.be.length(3);
+    });
+    it('should return [(1,1),(1,2),(1,3)] for aStar((1,1), (1,3))', () => {
+      expect(aStar(new Cell(1, 1),
+                   new Cell(3, 3),
+                   grid))
+        .to.be.length(5);
     });
   });
 });
