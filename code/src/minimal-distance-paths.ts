eda77f2919761965b70cf0304c23a72585205559
Eric Chase <eric.nalley+github@gmail.com>
2018-12-21 08:34:40 -0600

    separate PathSet out of minimal-distance-paths

--- code/src/minimal-distance-paths.ts
+++ code/src/minimal-distance-paths.ts
@@ -1,40 +1,8 @@
 import { Cell } from './grid';
 import { aStar } from './a-star';
 import { AStarGrid } from './a-star-grid';
-import { makePairs } from './permutation-function';
+import { PathSet } from './pathset';
 
-export class PathSet {
-  readonly pairs: Cell[][];
-  readonly paths: Cell[][] = [];
-
-  constructor(readonly endpoints: Cell[], readonly order: number[]) {
-    this.pairs = makePairs(endpoints);
-  }
-
-  * nextEndpointPair() {
-    yield this.pairs[this.order[0]];
-    yield this.pairs[this.order[1]];
-    yield this.pairs[this.order[2]];
-  };
-
-  storeNextPath(path: Cell[]): void {
-    if (this.paths.length === 3)
-      throw new Error('PathSet.storeNextPath: trying to store more than 3 paths');
-    this.paths.push(path);
-  }
-
-  getCombinedPaths(): Cell[] {
-    // swap each index with its value in order
-    let order: number[] = [];
-    order[this.order[0]] = 0;
-    order[this.order[1]] = 1;
-    order[this.order[2]] = 2;
-
-    return this.paths[order[0]]
-      .concat(this.paths[order[1]].slice(1))
-      .concat(this.paths[order[2]].slice(1));
-  }
-}
 
 export function getMinimalDistancePath(pathSet: PathSet) {
   let board: AStarGrid = new AStarGrid(10, 10);
--- code/test/minimal-distance-paths.test.ts
+++ code/test/minimal-distance-paths.test.ts
@@ -2,90 +2,10 @@ import { expect } from 'chai';
 import 'mocha';
 
 import { Cell } from '../src/grid';
-import { getMinimalDistancePath, PathSet } from '../src/minimal-distance-paths';
+import { getMinimalDistancePath } from '../src/minimal-distance-paths';
 import { AStarGrid } from '../src/a-star-grid';
+import { PathSet } from '../src/pathset';
 
-describe('PathSet', function () {
-  let pathSet: PathSet;
-
-  beforeEach('prepare a Path Set', () => {
-    pathSet = new PathSet(
-      [new Cell(0, 0),
-       new Cell(1, 1),
-       new Cell(2, 2),
-       new Cell(3, 3)], [2, 0, 1]);
-  });
-
-  describe('constructor', function () {
-    it('should populate a pairs list with 3 pairs', () => {
-      expect(pathSet.pairs)
-        .to.be.length(3);
-    });
-    it('should store input cell list as pairs', () => {
-      expect(pathSet.pairs)
-        .to.deep.equal(
-        [
-          [new Cell(0, 0), new Cell(1, 1)],
-          [new Cell(1, 1), new Cell(2, 2)],
-          [new Cell(2, 2), new Cell(3, 3)]
-        ]);
-    });
-    it('should store input number list as order', () => {
-      expect(pathSet.order)
-        .to.deep.equal([2, 0, 1]);
-    });
-  });
-
-  describe('* nextEndpointPair', function () {
-    it('should return endpoint pairs in the order specified by order', () => {
-      let endpointPairs = pathSet.nextEndpointPair();
-
-      expect(endpointPairs.next().value)
-        .to.deep.equal([new Cell(2, 2), new Cell(3, 3)]);
-      expect(endpointPairs.next().value)
-        .to.deep.equal([new Cell(0, 0), new Cell(1, 1)]);
-      expect(endpointPairs.next().value)
-        .to.deep.equal([new Cell(1, 1), new Cell(2, 2)]);
-    });
-  });
-
-  describe('storeNextPath', function () {
-    it('should store paths in the order specified by order', () => {
-      pathSet.storeNextPath([new Cell(2, 2), new Cell(2, 3), new Cell(3, 3)]);
-      pathSet.storeNextPath([new Cell(0, 0), new Cell(0, 1), new Cell(1, 1)]);
-      pathSet.storeNextPath([new Cell(1, 1), new Cell(1, 2), new Cell(2, 2)]);
-
-      expect(pathSet.paths)
-        .to.deep.equal(
-        [
-          [new Cell(2, 2), new Cell(2, 3), new Cell(3, 3)],
-          [new Cell(0, 0), new Cell(0, 1), new Cell(1, 1)],
-          [new Cell(1, 1), new Cell(1, 2), new Cell(2, 2)]
-        ]);
-    });
-    it('should throw error if trying to store more than 3 paths', () => {
-      pathSet.storeNextPath([new Cell(2, 2), new Cell(2, 3), new Cell(3, 3)]);
-      pathSet.storeNextPath([new Cell(0, 0), new Cell(0, 1), new Cell(1, 1)]);
-      pathSet.storeNextPath([new Cell(1, 1), new Cell(1, 2), new Cell(2, 2)]);
-
-      expect(() => pathSet.storeNextPath([new Cell(1, 1), new Cell(1, 2), new Cell(2, 2)]))
-        .to.throw(Error);
-    });
-  });
-
-  describe('getCombinedPaths', function () {
-    it('should return a combined path of paths in order of original end-points', () => {
-      pathSet.storeNextPath([new Cell(2, 2), new Cell(2, 3), new Cell(3, 3)]);
-      pathSet.storeNextPath([new Cell(0, 0), new Cell(0, 1), new Cell(1, 1)]);
-      pathSet.storeNextPath([new Cell(1, 1), new Cell(1, 2), new Cell(2, 2)]);
-
-      expect(pathSet.getCombinedPaths())
-        .to.deep.equal([new Cell(0, 0), new Cell(0, 1), new Cell(1, 1),
-                        new Cell(1, 2), new Cell(2, 2),
-                        new Cell(2, 3), new Cell(3, 3)]);
-    });
-  });
-});
 
 describe('Minimal Distance Path', function () {
   let pathSet: PathSet;
