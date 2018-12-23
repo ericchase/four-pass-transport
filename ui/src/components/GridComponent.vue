ca739e0151c7177a33a0be8ccfea1ee193da0e95
Christian Reyes <creyes922@gmail.com>
2018-12-23 12:50:25 -0600

    Set up a basic grid layout.

---
+++ ui/src/components/GridComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div class="grid-container" v-for="i in 10">
+      <div class="grid-item" v-for="j in 10"> {{i - 1}}{{j - 1}} </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class GridComponent extends Vue {
+}
+</script>
+
+<style scoped>
+  .grid-container {
+    display: grid;
+    grid-template-columns: auto auto auto auto auto auto auto auto auto auto;
+    background-color: grey
+  }
+
+  .grid-item {
+    background-color: rgba(255, 255, 255, 0.8);
+    border: 5px solid gray;
+    font-size: 10px;
+    text-align: center;
+    height: 100px;
+    width: 100px;
+  }
+</style>
--- ui/src/views/Home.vue
+++ ui/src/views/Home.vue
@@ -1,17 +1,28 @@
 <template>
   <div class="home">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <GridComponent></GridComponent>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import HelloWorld from '../components/HelloWorld.vue';
+import GridComponent from '../components/GridComponent.vue';
 
 @Component({
   components: {
     HelloWorld,
+    GridComponent,
   },
 })
 export default class Home extends Vue {}
 </script>
+
+<style scoped>
+  .center-stage{
+    margin: auto;
+    width: 80%;
+    padding: 10px;
+  }
+</style>
