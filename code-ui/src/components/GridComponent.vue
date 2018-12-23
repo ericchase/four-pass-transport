f0b55b4fb2fea85be50b6bd8eaee4280ba1d3b2e
Christian Reyes <creyes922@gmail.com>
2018-12-23 13:37:14 -0600

    Made some changes to the width

--- code-ui/src/components/GridComponent.vue
+++ code-ui/src/components/GridComponent.vue
@@ -1,32 +1,55 @@
 <template>
   <div>
-    <div class="grid-container" v-for="i in 10">
-      <div class="grid-item" v-for="j in 10"> {{i - 1}}{{j - 1}} </div>
+    <div v-for="i in 10">
+      <div class="square" v-for="j in 10">
+        <div class="content">
+          <div class="table">
+            <div class="table-cell">
+              {{i - 1}}{{j - 1}}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
-<script>
-import { Component, Prop, Vue } from 'vue-property-decorator';
+<script lang="ts">
+  import {Component, Vue} from 'vue-property-decorator';
 
-@Component
-export default class GridComponent extends Vue {
-}
+  @Component
+  export default class GridComponent extends Vue {
+  }
 </script>
 
 <style scoped>
-  .grid-container {
-    display: grid;
-    grid-template-columns: auto auto auto auto auto auto auto auto auto auto;
-    background-color: grey
+  .square {
+    float: left;
+    position: relative;
+    width: 9%;
+    padding-bottom: 9%; /* = width for a 1:1 aspect ratio */
+    margin: .5%;
+    overflow: hidden;
   }
 
-  .grid-item {
-    background-color: rgba(255, 255, 255, 0.8);
-    border: 5px solid gray;
-    font-size: 10px;
-    text-align: center;
-    height: 100px;
-    width: 100px;
+  .content {
+    background-color: grey;
+    position: absolute;
+    height: 99%; /* = 100% - 2*10% padding */
+    width: 99%; /* = 100% - 2*5% padding */
+    padding: 1% 1%;
+  }
+
+  .table {
+    display: table;
+    height: 100%;
+    width: 100%;
+  }
+
+  .table-cell {
+    display: table-cell;
+    vertical-align: middle;
+    height: 100%;
+    width: 100%;
   }
 </style>
